a9ed639d422327face1fc709c3c8ef73b2b65eef
goayasushi <goayasushi@gmail.com>
2023-03-23 08:44:39 +0900

    バリデーション設定

--- app/models/part.rb
+++ app/models/part.rb
@@ -2,4 +2,9 @@ class Part < ApplicationRecord
   belongs_to :user
   belongs_to :client
 
+  validates :part_name, presence: true
+  with_options presence: true, format: { with: /\A\d*\z/ } do
+    validates :purchase_price, numericality: { only_integer: true }
+    validates :selling_price, numericality: { only_integer: true }
+  end
 end
