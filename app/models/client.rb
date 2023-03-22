19f3a103e8a0e915a440b392d69ad9acfe3dbe26
goayasushi <goayasushi@gmail.com>
2023-03-22 12:04:14 +0900

    バリデーション設定

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,14 @@ class Client < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+
+  with_options presence: true do
+    validates :client_name
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :city
+    validates :block
+    validates :phone, format: {with: /\A\d{10,11}\z/ }
+  end
+  validates :prefecture_id, numericality: {other_than: 1}
+
 end
