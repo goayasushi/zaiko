ac9b1266ad4e1f42e8e7e2ef5b4167ddf31e5ba7
goayasushi <goayasushi@gmail.com>
2023-03-22 16:18:28 +0900

    仕入先登録機能実装

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,5 +8,17 @@ class ClientsController < ApplicationController
   end
   
   def create
+    @client = Client.new(client_params)
+    if @client.save
+      redirect_to clients_path
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def client_params
+    params.require(:client).permit(:client_name, :postcode, :prefecture_id, :city, :block, :building, :phone, :pic).merge(user_id: current_user.id)
   end
 end
