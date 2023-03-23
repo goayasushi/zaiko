bae209ed5f4c742693d6dc98807ef0abc43bce09
goayasushi <goayasushi@gmail.com>
2023-03-23 12:48:35 +0900

    出荷登録機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,4 +2,19 @@ class PurchasesController < ApplicationController
   def new
     @purchase = Purchase.new
   end
+
+  def create
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.save
+      redirect_to clients_path
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def purchase_params
+    params.require(:purchase).permit(:part_id, :arrival_day, :purchase_quantity).merge(user_id: current_user.id)
+  end
 end
