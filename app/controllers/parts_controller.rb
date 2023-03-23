0736654e83109b2b89f64f9f70840afc9754c8b4
goayasushi <goayasushi@gmail.com>
2023-03-23 09:44:13 +0900

    部品登録機能実装

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -2,4 +2,19 @@ class PartsController < ApplicationController
   def new
     @part = Part.new
   end
+
+  def create
+    @part = Part.new(part_params)
+    if @part.save
+      redirect_to clients_path
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def part_params
+    params.require(:part).permit(:part_name, :client_id, :purchase_price, :selling_price).merge(user_id: current_user.id)
+  end
 end
