7a825b72edd0512f793e1e8a466e358f6fb21cce
goayasushi <goayasushi@gmail.com>
2023-03-23 16:01:14 +0900

    purchaseモデルテストコード実装

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :purchase do
-    
+    part_id { 1 }
+    arrival_day {2023-03-23}
+    purchase_quantity {1}
+    association :user
+    association :part
   end
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchase = FactoryBot.build(:purchase)
+  end
+
+  describe '入荷登録' do
+    context '入荷登録できる場合' do
+      it 'purchase_id,arrival_day,purchase_quantity,user_idが存在していれば保存できる' do
+        expect(@purchase).to be_valid
+      end
+    end
+    
+    context '入荷登録できない場合' do
+      it 'part_idが空では保存できない' do
+        @purchase.part = nil
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Part must exist")
+      end
+      it 'arrival_dayが空では保存できない' do
+        @purchase.arrival_day = ''
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Arrival day can't be blank")
+      end
+      it 'purchase_quantityが空では保存できない' do
+        @purchase.purchase_quantity = ''
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Purchase quantity can't be blank")
+      end
+      it 'purchase_quantityが正の少数では保存できない' do
+        @purchase.purchase_quantity = Faker::Number.between(from: 1.0, to: 9999999.0)
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Purchase quantity must be an integer")
+      end
+      it 'purchase_quantityが0以下では保存できない' do
+        @purchase.purchase_quantity = -1
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Purchase quantity is invalid")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @purchase.user = nil
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
