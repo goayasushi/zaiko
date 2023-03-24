0897324bf10960287a7422274d2888f7eb842609
goayasushi <goayasushi@gmail.com>
2023-03-24 09:11:23 +0900

    バリデーション設定

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,9 @@
 class Sale < ApplicationRecord
   belongs_to :user
   belongs_to :part
+
+  validates :shipping_day, presence: true
+  with_options presence: true, format: { with: /\A\d*\z/ } do
+    validates :sale_quantity, numericality: { only_integer: true }
+  end
 end
