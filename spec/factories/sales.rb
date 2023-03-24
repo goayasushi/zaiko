519bf525e84a29656e815b7107199e3d4f20707b
goayasushi <goayasushi@gmail.com>
2023-03-24 09:35:29 +0900

    saleモデルテストコード実装

--- spec/factories/sales.rb
+++ spec/factories/sales.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :sale do
-    
+    part_id { 1 }
+    shipping_day {2023-03-23}
+    sale_quantity {1}
+    association :user
+    association :part
   end
 end
--- spec/models/sale_spec.rb
+++ spec/models/sale_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Sale, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @sale = FactoryBot.build(:sale)
+  end
+
+  describe '出荷登録' do
+    context '出荷登録できる場合' do
+      it 'sale_id,shipping_day,sale_quantity,user_idが存在していれば保存できる' do
+        expect(@sale).to be_valid
+      end
+    end
+    
+    context '出荷登録できない場合' do
+      it 'part_idが空では保存できない' do
+        @sale.part = nil
+        @sale.valid?
+        expect(@sale.errors.full_messages).to include("Part must exist")
+      end
+      it 'shipping_dayが空では保存できない' do
+        @sale.shipping_day = ''
+        @sale.valid?
+        expect(@sale.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it 'sale_quantityが空では保存できない' do
+        @sale.sale_quantity = ''
+        @sale.valid?
+        expect(@sale.errors.full_messages).to include("Sale quantity can't be blank")
+      end
+      it 'sale_quantityが正の少数では保存できない' do
+        @sale.sale_quantity = Faker::Number.between(from: 1.0, to: 9999999.0)
+        @sale.valid?
+        expect(@sale.errors.full_messages).to include("Sale quantity must be an integer")
+      end
+      it 'sale_quantityが0以下では保存できない' do
+        @sale.sale_quantity = -1
+        @sale.valid?
+        expect(@sale.errors.full_messages).to include("Sale quantity is invalid")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @sale.user = nil
+        @sale.valid?
+        expect(@sale.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
