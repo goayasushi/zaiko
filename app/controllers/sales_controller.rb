a1a004e59d9d6cba01b761f360c4d11af442d537
goayasushi <goayasushi@gmail.com>
2023-03-24 09:28:42 +0900

    出荷登録機能実装

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,4 +2,19 @@ class SalesController < ApplicationController
   def new
     @sale = Sale.new
   end
+
+  def create
+    @sale = Sale.new(sale_params)
+    if @sale.save
+      redirect_to clients_path
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def sale_params
+    params.require(:sale).permit(:part_id, :shipping_day, :sale_quantity).merge(user_id: current_user.id)
+  end
 end
